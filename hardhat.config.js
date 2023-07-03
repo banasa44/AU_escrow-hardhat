d735be695c5be7a9e8b05e9cdca7d380aca72958
banasa44 <cfbennassar@gmail.com>
2023-07-03 15:57:53 +0200

    initialization with sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
 require('@nomicfoundation/hardhat-toolbox');
+
+const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.17",
   paths: {
     artifacts: "./app/src/artifacts",
-  }
+  },
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
 };
